061ba9905d0d35c5810dc38e3f6b6a9547930b19
YuliiaDikun <dikun.yulia@gmail.com>
2024-03-23 15:49:35 +0200

    add types and typed computed props

---
+++ src/types/Emoji.ts
@@ -0,0 +1,17 @@
+type Emoji = 
+| "confused"
+| "cool"
+| "crying"
+| "grinning-squinting"
+| "happy"
+| "heart-eyes"
+| "laugh"
+| "sad"
+| "shoched"
+| "smiling"
+| "smiling-with-eyes"
+| "suprised"
+| "wink"
+| "worried"
+
+export default Emoji;
