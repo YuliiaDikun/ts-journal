301aced3c429eddbf59e5e3493141fe14e713dce
YuliiaDikun <dikun.yulia@gmail.com>
2024-03-23 15:58:17 +0200

    add limitation for textarea

--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -1,22 +1,34 @@
 <script lang="ts" setup>
-import EmojiField from "@/components/EmojiField.vue";
-import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
+import EmojiField from '@/components/EmojiField.vue';
+import ArrowCircleRight from '@/assets/icons/arrow-circle-right.svg';
 import type Emoji from '@/types/Emoji';
-import {ref, computed} from 'vue';
+import { ref, computed } from 'vue';
 
 const text = ref('');
-const emoji= ref<Emoji | null>(null);
+const emoji = ref<Emoji | null>(null);
 const charCount = computed(() => text.value.length);
+const maxChars = 280;
+
+const handleTextInput = (e: Event) => {
+  const textarea = e.target as HTMLTextAreaElement;
+  if (textarea.value.length <= 280){
+    text.value = textarea.value;
+  } else {
+    text.value = textarea.value = textarea.value.substring(0, maxChars);
+  }
+};
 </script>
 
 <template>
   <form class="entry-form" @submit.prevent>
-    <textarea 
-    v-model="text"
-    placeholder="New Journal Entry for danielkelly_io"></textarea>
-    <EmojiField v-model="emoji"/>
+    <textarea
+      :value="text"
+      @keyup="handleTextInput"
+      placeholder="New Journal Entry for danielkelly_io"
+    ></textarea>
+    <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
-      <span>{{ charCount }} / 280</span>
+      <span>{{ charCount }} / {{ maxChars }}</span>
       <button>Remember <ArrowCircleRight width="20" /></button>
     </div>
   </form>
